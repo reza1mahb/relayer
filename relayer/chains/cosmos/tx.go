reza1mahb/relayer

commit ea42d5de0ee5c78f5f7d8ec0480a43c10de387a9

add build evm

relayer/chains/cosmos/tx.go
@@ -2,16 +2,20 @@ package cosmos
 
 import (
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"math"
 	"math/big"
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	errorsmod "cosmossdk.io/errors"
+	sdkmath "cosmossdk.io/math"
 	"github.com/avast/retry-go/v4"
 	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cometbft/cometbft/light"
@@ -29,6 +33,9 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/cosmos/gogoproto/proto"
 	feetypes "github.com/cosmos/ibc-go/v7/modules/apps/29-fee/types"
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
@@ -40,8 +47,13 @@ import (
 	tmclient "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 	localhost "github.com/cosmos/ibc-go/v7/modules/light-clients/09-localhost"
 	strideicqtypes "github.com/cosmos/relayer/v2/relayer/chains/cosmos/stride"
-	"github.com/cosmos/relayer/v2/relayer/ethermint"
+	ethermintcodecs "github.com/cosmos/relayer/v2/relayer/codecs/ethermint"
 	"github.com/cosmos/relayer/v2/relayer/provider"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+	etherminttypes "github.com/evmos/ethermint/types"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -143,8 +155,20 @@ func (cc *CosmosProvider) SendMessagesToMempool(
 	// the account sequence query all the way through the transaction broadcast success/fail.
 	cc.txMu.Lock()
 	defer cc.txMu.Unlock()
-
-	txBytes, sequence, fees, err := cc.buildMessages(ctx, msgs, memo)
+	txf, txb, err := cc.getTxFactoryAndBuilder(ctx, msgs, memo)
+	if err != nil {
+		return err
+	}
+	done := cc.SetSDKContext()
+	var txBytes []byte
+	var sequence uint64
+	var fees sdk.Coins
+	if len(cc.PCfg.PrecompiledContractAddress) > 0 {
+		txBytes, sequence, fees, err = cc.buildEvmMessages(ctx, txf, txb)
+	} else {
+		txBytes, sequence, fees, err = cc.buildMessages(ctx, txf, txb)
+	}
+	done()
 	if err != nil {
 		// Account sequence mismatch errors can happen on the simulated transaction also.
 		if strings.Contains(err.Error(), sdkerrors.ErrWrongSequence.Error()) {
@@ -155,13 +179,14 @@ func (cc *CosmosProvider) SendMessagesToMempool(
 	}
 
 	if err := cc.broadcastTx(ctx, txBytes, msgs, fees, asyncCtx, defaultBroadcastWaitTimeout, asyncCallback); err != nil {
+		cc.log.Info("broadcast fail", zap.Error(err))
 		if strings.Contains(err.Error(), sdkerrors.ErrWrongSequence.Error()) {
 			cc.handleAccountSequenceMismatchError(err)
 		}
 
 		return err
 	}
-
+	cc.log.Info("broadcasted")
 	// we had a successful tx broadcast with this sequence, so update it to the next
 	cc.updateNextAccountSequence(sequence + 1)
 
@@ -345,14 +370,18 @@ func parseEventsFromTxResponse(resp *sdk.TxResponse) []provider.RelayerEvent {
 	return events
 }
 
-func (cc *CosmosProvider) buildMessages(ctx context.Context, msgs []provider.RelayerMessage, memo string) ([]byte, uint64, sdk.Coins, error) {
+func (cc *CosmosProvider) getTxFactoryAndBuilder(
+	ctx context.Context,
+	msgs []provider.RelayerMessage,
+	memo string,
+) (*tx.Factory, client.TxBuilder, error) {
 	// Query account details
 	txf, err := cc.PrepareFactory(cc.TxFactory())
 	if err != nil {
-		return nil, 0, sdk.Coins{}, err
+		return nil, nil, err
 	}
-
-	if memo != "" {
+	cc.log.Info("memo", zap.String("origin", memo))
+	if len(cc.PCfg.PrecompiledContractAddress) == 0 && memo != "" {
 		txf = txf.WithMemo(memo)
 	}
 
@@ -362,14 +391,13 @@ func (cc *CosmosProvider) buildMessages(ctx context.Context, msgs []provider.Rel
 		sequence = cc.nextAccountSeq
 		txf = txf.WithSequence(sequence)
 	}
-
 	// TODO: Make this work with new CalculateGas method
 	// TODO: This is related to GRPC client stuff?
 	// https://github.com/cosmos/cosmos-sdk/blob/5725659684fc93790a63981c653feee33ecf3225/client/tx/tx.go#L297
 	// If users pass gas adjustment, then calculate gas
 	_, adjusted, err := cc.CalculateGas(ctx, txf, CosmosMsgs(msgs...)...)
 	if err != nil {
-		return nil, 0, sdk.Coins{}, err
+		return nil, nil, err
 	}
 
 	// Set the gas amount on the transaction factory
@@ -384,25 +412,42 @@ func (cc *CosmosProvider) buildMessages(ctx context.Context, msgs []provider.Rel
 		}
 		return nil
 	}, retry.Context(ctx), rtyAtt, rtyDel, rtyErr); err != nil {
-		return nil, 0, sdk.Coins{}, err
+		return nil, nil, err
 	}
 
-	done := cc.SetSDKContext()
+	return &txf, txb, nil
+}
 
-	if err := retry.Do(func() error {
-		if err := tx.Sign(txf, cc.PCfg.Key, txb, false); err != nil {
-			return err
-		}
-		return nil
-	}, retry.Context(ctx), rtyAtt, rtyDel, rtyErr); err != nil {
-		return nil, 0, sdk.Coins{}, err
+func convertAddress(addrString string) (*common.Address, error) {
+	cfg := sdk.GetConfig()
+	var addr []byte
+	// try hex, then bech32
+	switch {
+	case common.IsHexAddress(addrString):
+		addr = common.HexToAddress(addrString).Bytes()
+	case strings.HasPrefix(addrString, cfg.GetBech32ValidatorAddrPrefix()):
+		addr, _ = sdk.ValAddressFromBech32(addrString)
+	case strings.HasPrefix(addrString, cfg.GetBech32AccountAddrPrefix()):
+		addr, _ = sdk.AccAddressFromBech32(addrString)
+	default:
+		return nil, fmt.Errorf("expected a valid hex or bech32 address (acc prefix %s), got '%s'", cfg.GetBech32AccountAddrPrefix(), addrString)
 	}
+	to := common.BytesToAddress(addr)
+	return &to, nil
+}
 
-	done()
+func convertCoins(prices string) (sdk.Coins, error) {
+	coins, err := sdk.ParseCoinsNormalized(prices)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "failed to parse coins")
+	}
+	return coins, nil
+}
 
-	tx := txb.GetTx()
-	fees := tx.GetFee()
-
+func (cc *CosmosProvider) signTxBytes(
+	ctx context.Context,
+	tx authsigning.Tx,
+) ([]byte, error) {
 	var txBytes []byte
 	// Generate the transaction bytes
 	if err := retry.Do(func() error {
@@ -413,10 +458,213 @@ func (cc *CosmosProvider) buildMessages(ctx context.Context, msgs []provider.Rel
 		}
 		return nil
 	}, retry.Context(ctx), rtyAtt, rtyDel, rtyErr); err != nil {
+		return nil, err
+	}
+	return txBytes, nil
+}
+
+func (cc *CosmosProvider) buildMessages(ctx context.Context, txf *tx.Factory, txb client.TxBuilder) ([]byte, uint64, sdk.Coins, error) {
+	if err := retry.Do(func() error {
+		if err := tx.Sign(*txf, cc.PCfg.Key, txb, false); err != nil {
+			return err
+		}
+		return nil
+	}, retry.Context(ctx), rtyAtt, rtyDel, rtyErr); err != nil {
 		return nil, 0, sdk.Coins{}, err
 	}
 
-	return txBytes, sequence, fees, nil
+	tx := txb.GetTx()
+	txBytes, err := cc.signTxBytes(ctx, tx)
+	if err != nil {
+		return nil, 0, sdk.Coins{}, err
+	}
+
+	return txBytes, txf.Sequence(), tx.GetFee(), nil
+}
+
+func getChainConfig(chainID *big.Int) *params.ChainConfig {
+	homesteadBlock := new(big.Int)
+	daoForkBlock := new(big.Int)
+	eip150Block := new(big.Int)
+	eip155Block := new(big.Int)
+	eip158Block := new(big.Int)
+	byzantiumBlock := new(big.Int)
+	constantinopleBlock := new(big.Int)
+	petersburgBlock := new(big.Int)
+	istanbulBlock := new(big.Int)
+	muirGlacierBlock := new(big.Int)
+	berlinBlock := new(big.Int)
+	londonBlock := new(big.Int)
+	arrowGlacierBlock := new(big.Int)
+	grayGlacierBlock := new(big.Int)
+	mergeNetsplitBlock := new(big.Int)
+	shanghaiBlock := new(big.Int)
+	cancunBlock := new(big.Int)
+
+	return &params.ChainConfig{
+		ChainID:             chainID,
+		HomesteadBlock:      homesteadBlock,
+		DAOForkBlock:        daoForkBlock,
+		DAOForkSupport:      true,
+		EIP150Block:         eip150Block,
+		EIP155Block:         eip155Block,
+		EIP158Block:         eip158Block,
+		ByzantiumBlock:      byzantiumBlock,
+		ConstantinopleBlock: constantinopleBlock,
+		PetersburgBlock:     petersburgBlock,
+		IstanbulBlock:       istanbulBlock,
+		MuirGlacierBlock:    muirGlacierBlock,
+		BerlinBlock:         berlinBlock,
+		LondonBlock:         londonBlock,
+		ArrowGlacierBlock:   arrowGlacierBlock,
+		GrayGlacierBlock:    grayGlacierBlock,
+		MergeNetsplitBlock:  mergeNetsplitBlock,
+		ShanghaiBlock:       shanghaiBlock,
+		CancunBlock:         cancunBlock,
+	}
+}
+
+// prefix bytes for the relayer msg type
+const (
+	prefixSize4Bytes = 4
+	// Client
+	prefixCreateClient = iota + 1
+	prefixUpdateClient
+	prefixUpgradeClient
+	prefixSubmitMisbehaviour
+	// Connection
+	prefixConnectionOpenInit
+	prefixConnectionOpenTry
+	prefixConnectionOpenAck
+	prefixConnectionOpenConfirm
+	// Channel
+	prefixChannelOpenInit
+	prefixChannelOpenTry
+	prefixChannelOpenAck
+	prefixChannelOpenConfirm
+	prefixChannelCloseInit
+	prefixChannelCloseConfirm
+	prefixRecvPacket
+	prefixAcknowledgement
+	prefixTimeout
+	prefixTimeoutOnClose
+)
+
+var messageMap = map[reflect.Type]int{
+	reflect.TypeOf((*clienttypes.MsgCreateClient)(nil)):        prefixCreateClient,
+	reflect.TypeOf((*clienttypes.MsgUpdateClient)(nil)):        prefixUpdateClient,
+	reflect.TypeOf((*clienttypes.MsgUpgradeClient)(nil)):       prefixUpgradeClient,
+	reflect.TypeOf((*clienttypes.MsgSubmitMisbehaviour)(nil)):  prefixSubmitMisbehaviour,
+	reflect.TypeOf((*conntypes.MsgConnectionOpenInit)(nil)):    prefixConnectionOpenInit,
+	reflect.TypeOf((*conntypes.MsgConnectionOpenTry)(nil)):     prefixConnectionOpenTry,
+	reflect.TypeOf((*conntypes.MsgConnectionOpenAck)(nil)):     prefixConnectionOpenAck,
+	reflect.TypeOf((*conntypes.MsgConnectionOpenConfirm)(nil)): prefixConnectionOpenConfirm,
+	reflect.TypeOf((*chantypes.MsgChannelOpenInit)(nil)):       prefixChannelOpenInit,
+	reflect.TypeOf((*chantypes.MsgChannelOpenTry)(nil)):        prefixChannelOpenTry,
+	reflect.TypeOf((*chantypes.MsgChannelOpenAck)(nil)):        prefixChannelOpenAck,
+	reflect.TypeOf((*chantypes.MsgChannelOpenConfirm)(nil)):    prefixChannelOpenConfirm,
+	reflect.TypeOf((*chantypes.MsgChannelCloseInit)(nil)):      prefixChannelCloseInit,
+	reflect.TypeOf((*chantypes.MsgChannelCloseConfirm)(nil)):   prefixChannelCloseConfirm,
+	reflect.TypeOf((*chantypes.MsgRecvPacket)(nil)):            prefixRecvPacket,
+	reflect.TypeOf((*chantypes.MsgAcknowledgement)(nil)):       prefixAcknowledgement,
+	reflect.TypeOf((*chantypes.MsgTimeout)(nil)):               prefixTimeout,
+	reflect.TypeOf((*chantypes.MsgTimeoutOnClose)(nil)):        prefixTimeoutOnClose,
+}
+
+func addLengthPrefix(prefix int, input []byte) []byte {
+	prefixBytes := make([]byte, prefixSize4Bytes)
+	binary.LittleEndian.PutUint32(prefixBytes, uint32(prefix))
+	return append(prefixBytes, input...)
+}
+
+func (cc *CosmosProvider) buildEvmMessages(ctx context.Context, txf *tx.Factory, txb client.TxBuilder) ([]byte, uint64, sdk.Coins, error) {
+	chainID, err := ethermintcodecs.ParseChainID(cc.PCfg.ChainID)
+	if err != nil {
+		return nil, 0, sdk.Coins{}, err
+	}
+	gasLimit := txf.Gas()
+	gasPrices, err := convertCoins(cc.PCfg.GasPrices)
+	if err != nil {
+		return nil, 0, sdk.Coins{}, err
+	}
+	gasPriceCoin := gasPrices[0]
+	gasPrice := gasPriceCoin.Amount.BigInt()
+	gasFeeCap := gasPrice
+	var gasTipCap *big.Int
+	if len(cc.PCfg.ExtensionOptions) > 0 {
+		var ok bool
+		gasTipCap, ok = new(big.Int).SetString(cc.PCfg.ExtensionOptions[0].Value, 10)
+		if !ok {
+			return nil, 0, sdk.Coins{}, errors.New("unsupported extOption")
+		}
+	}
+	txs := make([]sdk.Msg, 0, len(txb.GetTx().GetMsgs()))
+	fees := make(sdk.Coins, 0)
+	var txGasLimit uint64 = 0
+	contractAddress := common.HexToAddress(cc.PCfg.PrecompiledContractAddress)
+	blockNumber := new(big.Int)
+	for i, m := range txb.GetTx().GetMsgs() {
+		prefix, ok := messageMap[reflect.TypeOf(m)]
+		if !ok {
+			return nil, 0, sdk.Coins{}, errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "invalid message type %T", m)
+		}
+		signers := m.GetSigners()
+		if len(signers) != 1 {
+			return nil, 0, sdk.Coins{}, errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "invalid signers length %d", len(signers))
+		}
+		from, err := convertAddress(signers[0].String())
+		if err != nil {
+			return nil, 0, sdk.Coins{}, err
+		}
+		input, err := proto.Marshal(m)
+		if err != nil {
+			return nil, 0, sdk.Coins{}, err
+		}
+		nonce := txf.Sequence() + uint64(i)
+		amount := big.NewInt(0)
+		input = addLengthPrefix(prefix, input)
+		tx := evmtypes.NewTx(chainID, nonce, &contractAddress, amount, gasLimit, gasFeeCap, gasPrice, gasTipCap, input, &ethtypes.AccessList{})
+		tx.From = from.String()
+		if err := tx.ValidateBasic(); err != nil {
+			cc.log.Info("tx failed basic validation", zap.Error(err))
+			return nil, 0, sdk.Coins{}, err
+		}
+		if err := retry.Do(func() error {
+			signer := ethtypes.MakeSigner(getChainConfig(chainID), blockNumber)
+			if err := tx.Sign(signer, cc.Keybase); err != nil {
+				return err
+			}
+			return nil
+		}, retry.Context(ctx), rtyAtt, rtyDel, rtyErr); err != nil {
+			return nil, 0, sdk.Coins{}, err
+		}
+		if feeAmt := sdkmath.NewIntFromBigInt(tx.GetFee()); feeAmt.Sign() > 0 {
+			fees = fees.Add(sdk.NewCoin(gasPriceCoin.Denom, feeAmt))
+		}
+		txGasLimit += tx.GetGas()
+		tx.From = ""
+		cc.log.Info("append", zap.String("hash", tx.Hash))
+		txs = append(txs, tx)
+	}
+
+	builder, ok := txb.(authtx.ExtensionOptionsTxBuilder)
+	if !ok {
+		return nil, 0, sdk.Coins{}, errors.New("unsupported builder")
+	}
+
+	option, err := types.NewAnyWithValue(new(evmtypes.ExtensionOptionsEthereumTx))
+	if err != nil {
+		return nil, 0, sdk.Coins{}, err
+	}
+	builder.SetExtensionOptions(option)
+	builder.SetMsgs(txs...)
+	builder.SetFeeAmount(fees)
+	builder.SetGasLimit(txGasLimit)
+	txBytes, err := cc.signTxBytes(ctx, builder.GetTx())
+	if err != nil {
+		return nil, 0, sdk.Coins{}, err
+	}
+	return txBytes, txf.Sequence(), fees, nil
 }
 
 // handleAccountSequenceMismatchError will parse the error string, e.g.:
@@ -1411,7 +1659,7 @@ func (cc *CosmosProvider) SetWithExtensionOptions(txf tx.Factory) (tx.Factory, e
 		if !ok {
 			return txf, fmt.Errorf("invalid opt value")
 		}
-		extensionOption := ethermint.ExtensionOptionDynamicFeeTx{
+		extensionOption := etherminttypes.ExtensionOptionDynamicFeeTx{
 			MaxPriorityPrice: max,
 		}
 		extBytes, err := extensionOption.Marshal()

cregistry/chain_info.go
@@ -84,9 +84,10 @@ type ChainInfo struct {
 			Provider string `json:"provider"`
 		} `json:"rest"`
 	} `json:"apis"`
-	MaxGasAmount     uint64                     `json:"max_gas_amount"`
-	ExtraCodecs      []string                   `json:"extra_codecs"`
-	ExtensionOptions []provider.ExtensionOption `json:"extension_options"`
+	MaxGasAmount               uint64                     `json:"max_gas_amount"`
+	ExtraCodecs                []string                   `json:"extra_codecs"`
+	ExtensionOptions           []provider.ExtensionOption `json:"extension_options"`
+	PrecompiledContractAddress string                     `json:"precompiled_contract_address"`
 }
 
 // NewChainInfo returns a ChainInfo that is uninitialized other than the provided zap.Logger.
@@ -256,22 +257,23 @@ func (c ChainInfo) GetChainConfig(ctx context.Context) (*cosmos.CosmosProviderCo
 	}
 
 	return &cosmos.CosmosProviderConfig{
-		Key:              "default",
-		ChainID:          c.ChainID,
-		RPCAddr:          rpc,
-		AccountPrefix:    c.Bech32Prefix,
-		KeyringBackend:   "test",
-		GasAdjustment:    1.2,
-		GasPrices:        gasPrices,
-		KeyDirectory:     home,
-		Debug:            debug,
-		Timeout:          "20s",
-		OutputFormat:     "json",
-		SignModeStr:      "direct",
-		Slip44:           c.Slip44,
-		SigningAlgorithm: c.SigningAlgorithm,
-		ExtraCodecs:      c.ExtraCodecs,
-		MaxGasAmount:     c.MaxGasAmount,
-		ExtensionOptions: c.ExtensionOptions,
+		Key:                        "default",
+		ChainID:                    c.ChainID,
+		RPCAddr:                    rpc,
+		AccountPrefix:              c.Bech32Prefix,
+		KeyringBackend:             "test",
+		GasAdjustment:              1.2,
+		GasPrices:                  gasPrices,
+		KeyDirectory:               home,
+		Debug:                      debug,
+		Timeout:                    "20s",
+		OutputFormat:               "json",
+		SignModeStr:                "direct",
+		Slip44:                     c.Slip44,
+		SigningAlgorithm:           c.SigningAlgorithm,
+		ExtraCodecs:                c.ExtraCodecs,
+		MaxGasAmount:               c.MaxGasAmount,
+		ExtensionOptions:           c.ExtensionOptions,
+		PrecompiledContractAddress: c.PrecompiledContractAddress,
 	}, nil
 }

go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/cosmos/ibc-go/v7 v7.2.0
 	github.com/cosmos/ics23/go v0.10.0
 	github.com/ethereum/go-ethereum v1.10.26
+	github.com/evmos/ethermint v0.0.0-00010101000000-000000000000
 	github.com/gofrs/flock v0.8.1
 	github.com/google/go-cmp v0.5.9
 	github.com/google/go-github/v43 v43.0.0
@@ -50,7 +51,9 @@ require (
 	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4 // indirect
 	github.com/99designs/keyring v1.2.1 // indirect
-	github.com/ChainSafe/go-schnorrkel v0.0.0-20200405005733-88cbf1b4c40d // indirect
+	github.com/ChainSafe/go-schnorrkel v1.0.0 // indirect
+	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/VictoriaMetrics/fastcache v1.6.0 // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/aws/aws-sdk-go v1.44.203 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
@@ -74,6 +77,7 @@ require (
 	github.com/creachadair/taskgroup v0.4.2 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/dgraph-io/badger/v2 v2.2007.4 // indirect
@@ -83,10 +87,14 @@ require (
 	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -117,7 +125,8 @@ require (
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hdevalence/ed25519consensus v0.1.0 // indirect
-	github.com/holiman/uint256 v1.2.0 // indirect
+	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
+	github.com/holiman/uint256 v1.2.1 // indirect
 	github.com/huandu/skiplist v1.2.0 // indirect
 	github.com/improbable-eng/grpc-web v0.15.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -139,6 +148,7 @@ require (
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mtibben/percent v0.2.1 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/petermattis/goid v0.0.0-20230317030725-371a4b8eda08 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -146,11 +156,14 @@ require (
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/prometheus/tsdb v0.7.1 // indirect
 	github.com/rakyll/statik v0.1.7 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rs/cors v1.8.3 // indirect
 	github.com/rs/zerolog v1.29.1 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -159,6 +172,7 @@ require (
 	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
 	github.com/tidwall/btree v1.6.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.4.0 // indirect
 	github.com/ulikunitz/xz v0.5.11 // indirect
 	github.com/zondax/hid v0.9.1 // indirect
@@ -178,7 +192,10 @@ require (
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	nhooyr.io/websocket v1.8.6 // indirect
 	pgregory.net/rapid v0.5.5 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
+
+replace github.com/evmos/ethermint => github.com/mmsqe/ethermint v0.6.5-0.20230711022516-7597dac87b91

commit b88ba46df980582a6469bce181917dc0e6fb95f9

Problem: wrong price and feeCap when new evm tx
this bug was introduced in ea42d5de0ee5c78f5f7d8ec0480a43c10de387a9

relayer/chains/cosmos/tx.go
@@ -1024,7 +1024,7 @@ func (cc *CosmosProvider) buildEvmMessages(
 		}
 		nonce := txf.Sequence() + uint64(i)
 		amount := big.NewInt(0)
-		tx := evmtypes.NewTx(chainID, nonce, &contractAddress, amount, gasLimit, gasFeeCap, gasPrice, gasTipCap, is.Input, &ethtypes.AccessList{})
+		tx := evmtypes.NewTx(chainID, nonce, &contractAddress, amount, gasLimit, gasPrice, gasFeeCap, gasTipCap, is.Input, &ethtypes.AccessList{})
 		tx.From = from.Bytes()
 		if err := tx.ValidateBasic(); err != nil {
 			cc.log.Info("tx failed basic validation", zap.Error(err))